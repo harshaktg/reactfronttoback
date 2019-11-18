c0cc518d6447adab8c1fa9b0aab80fafff870097
Harsha Vardhan <harshaktg@gmail.com>
2019-11-18 23:16:10 +0530

    Updates

---
+++ precache-manifest.64ec967d339b2c92cc2f9ef30a375c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96b8f850e765e16cc069e5c1d957cae",
+    "url": "/reactfronttoback/index.html"
+  },
+  {
+    "revision": "b480dec07bcef50175de",
+    "url": "/reactfronttoback/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "78efb9218318c15460e8",
+    "url": "/reactfronttoback/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b480dec07bcef50175de",
+    "url": "/reactfronttoback/static/js/2.85b04485.chunk.js"
+  },
+  {
+    "revision": "78efb9218318c15460e8",
+    "url": "/reactfronttoback/static/js/main.2f15ccdd.chunk.js"
+  },
+  {
+    "revision": "27d526a7acc62c5beb3c",
+    "url": "/reactfronttoback/static/js/runtime-main.609377fb.js"
+  }
+]);
